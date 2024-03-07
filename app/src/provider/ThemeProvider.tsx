928a61c6366e4f9706e7699e06c29b1efe6b1848
NapolDeveloper <loma1016@gmail.com>
2024-03-07 23:24:27 +0900

    [Fix] ThemeProvider GlobalStyle 적용하도록 변경

--- app/src/provider/ThemeProvider.tsx
+++ app/src/provider/ThemeProvider.tsx
@@ -1,12 +1,17 @@
 import { PropsWithChildren } from 'react';
 import { ThemeProvider as StyledProvider } from 'styled-components';
+import GlobalStyles from 'styles/GlobalStyles';
 
 import { theme } from 'styles/theme';
 
 /** 테마 제공 컴포넌트 */
 const ThemeProvider = ({ children }: PropsWithChildren) => {
-  console.log(theme);
-  return <StyledProvider theme={theme}>{children}</StyledProvider>;
+  return (
+    <StyledProvider theme={theme}>
+      <GlobalStyles />
+      {children}
+    </StyledProvider>
+  );
 };
 
 export default ThemeProvider;
